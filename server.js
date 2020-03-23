e9e5405ac06f8b4851546153e0c3c60713b1e50d
Alan Elam <aelam757@gmail.com>
2020-03-23 00:47:36 -0400

    connection made

---
+++ server.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "trackerDB." 
+});
+
+connection.connect(err => {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+  
+  
+  
+  
+  module.exports = connection;
